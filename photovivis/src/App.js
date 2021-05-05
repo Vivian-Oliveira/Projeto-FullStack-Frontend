242aba6629248aba51363db4268c3c729ecb3bd1
Vivian-Oliveira <costa.vivs@gmail.com>
2021-05-05 01:42:57 +0100

    criei constantes das cores primaria e secundaria e base url

--- photovivis/src/App.js
+++ photovivis/src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Router  from "./routes/Router"
+import theme from "./constants/theme"
+import { ThemeProvider } from "@material-ui/styles"
 
 const App = () => {
   return (
-    <div >
-      <Router />
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <Router/>
+    </ThemeProvider> 
+  )
 }
 
 export default App;
--- photovivis/src/pages/ImageListPage/ImageListPage.js
+++ photovivis/src/pages/ImageListPage/ImageListPage.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import {Button} from "@material-ui/core/Button"
 
 const ImageListPage = (props) => {
-    return(
-        <div>
-            Feed Page 
-        </div>
-    )
-}
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+    </div>
+  );
+};
 
-export default ImageListPage
+export default ImageListPage;
